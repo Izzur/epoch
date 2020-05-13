a33c24bb7cc1d7286122df6741df9d10bc7e151e
Izzur Zuhri <izzur.zuhri@gmail.com>
2020-05-13 12:50:05 +0700

    Updates

---
+++ precache-manifest.5a76165abbeb4abb230d85eb75e90604.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a621342e57c323fc16e4469be0f50f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78051842e2637b5c387f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf7eab085d82fd8400d3",
+    "url": "/static/js/2.a0117901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0117901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78051842e2637b5c387f",
+    "url": "/static/js/main.19c2ac51.chunk.js"
+  },
+  {
+    "revision": "864e6bb46343394c9d08",
+    "url": "/static/js/runtime-main.a6ebf459.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
