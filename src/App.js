013337931576748867c73fba6c38f618eb1c360e
Izzur Zuhri <izzur.zuhri@gmail.com>
2020-05-13 14:14:20 +0700

    Add convert form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    const d = new Date(parseInt(e.target.value) * 1000);
+    this.setState({ date: d.toString() });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form>
+            <fieldset>
+              <label htmlFor="epoch">Epoch</label>
+              <input
+                className="padded"
+                type="number"
+                id="epoch"
+                onChange={this.handleChange}
+                size="64"
+              ></input>
+              <label htmlFor="date">Date</label>
+              <input
+                className="padded"
+                type="text"
+                id="date"
+                value={this.state.date}
+                readOnly
+                size="64"
+              ></input>
+            </fieldset>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
